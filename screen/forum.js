5932d037a499d0f6b070ede77c279e7229cf63b4
Tobi <naroto102hn2@gmail.com>
2020-12-19 23:57:59 +0700

    forum 1/3 done

--- screen/forum.js
+++ screen/forum.js
@@ -4,9 +4,13 @@ const style = `
 
 .forum-main {
     text-align: center;
-    display: 80%;
+    width: -webkit-fill-available;
     border: 1px solid #363636;
     margin: 2% 10%;
+}
+.main{
+    display:flex;
+    justify-content: space-between;
 }
 `
 class forumScreen extends HTMLElement{
@@ -21,8 +25,12 @@ class forumScreen extends HTMLElement{
         </style>
         <div class = "forum-container">
         <cnm-header></cnm-header>
+        <div class="main">
+        <img class="banner" src="../images/banner-qc.jpg" width="100px">
         <div class="forum-main">
             <avt-text></avt-text>
+        </div>
+        <img class="banner" src="../images/banner-qc.jpg" width="100px">
         </div>
         <cnm-footer></cnm-footer>
         </div>
